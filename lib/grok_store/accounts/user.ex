be9f49a8a02ea741780abc54b0862bff032e65e8
Greg Cline <gregrcline@gmail.com>
2019-06-13 21:41:04 -0400

    WIP just generated users

---
+++ lib/grok_store/accounts/user.ex
@@ -0,0 +1,19 @@
+defmodule GrokStore.Accounts.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :email, :string
+    field :name, :string
+    field :passhash, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:name, :email, :passhash])
+    |> validate_required([:name, :email, :passhash])
+  end
+end
